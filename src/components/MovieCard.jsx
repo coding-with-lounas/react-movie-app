df49aa0b52164d2d4caa1068b0f7dab829de8802
Lounas <lounasidjr@gmail.com>
2025-08-09 20:33:13 +0100

    add base url change

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,11 @@ const BASE_URL = import.meta.env.BASE_URL;
 const MovieCard = ({movie : {title,vote_average,poster_path,release_date,original_language}}) => {
   return (
     <div className='movie-card'>
-       <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : `${BASE_URL}No-Poster.png`} alt={title} />
+       <img 
+  src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : `${BASE_URL}No-Poster.png`} 
+  alt={title} 
+  onError={(e) => { e.currentTarget.src = `${BASE_URL}No-Poster.png`; }} 
+/>
        <div className="mt-4">
         <h3>{title}</h3>
         <div className="content">
