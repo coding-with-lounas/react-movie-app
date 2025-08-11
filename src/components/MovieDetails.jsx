02259bc7eb59ce5514b6d414c1ce0c76eb4f9e96
Lounas <lounasidjr@gmail.com>
2025-08-11 16:43:27 +0100

    come back

---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+
+
+// const MovieDetails = ({movie : {title,vote_average,poster_path,release_date,original_language}}) => {
+    
+//     return 
+ 
+// ;
+//   };
+//   export default MovieDetails;
